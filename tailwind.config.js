15b1708bc35d06f51bed97d1569b2f02f582e94a
kalvinwei007@gmail.com <kalvinwei007@gmail.com>
2021-10-03 18:46:45 +1300

    Add moment to project, so the time is up-to-date

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  purge: [],
+  mode:'jit', //just-in-time mode, a new feature with v2.1. Refer to: https://tailwindcss.com/docs/just-in-time-mode
+  purge: ['index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
